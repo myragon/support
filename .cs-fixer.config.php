6a7754ec01605f09cfdc8a67a3faaaa006fd4cac
bdumitrua <bdumitrua@gmail.com>
2025-07-21 03:22:13 +0300

    Initial commit for myragon/support

---
+++ .cs-fixer.config.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src');
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig(2, 20))
+    ->setRules([
+        '@PSR12' => true,
+        '@PHP84Migration' => true,
+        'strict_param' => true,
+    ])
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__ . './tmp/.php-cs-fixer.cache');
